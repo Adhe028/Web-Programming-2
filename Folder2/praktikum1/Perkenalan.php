b948c5e35074f4ac30cf4f9e641c5b093739ea16
Adhe028 <adhekurniawan028@gmail.com>
2020-10-20 13:06:26 +0700

    tugas

---
+++ Folder2/praktikum1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php 
+class Perkenalan extends CI_Controller{
+	public function index(){
+		echo "<h1>Perkenalkan<h1>";
+		echo "<h2>Nama saya Ade Kurniawan<br>
+			Saya tinggal didaerah Tegal<br>
+			Olahraga yang saya sukai adalah lari sambil jalan<h2>";
+	}
+}
+
+
